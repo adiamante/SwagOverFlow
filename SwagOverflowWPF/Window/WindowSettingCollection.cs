151d4ebc7a0612f5776b68e3ca5196d2c82f12a0
Desktop <dmnt123@gmail.com>
2020-03-05 02:22:45 -0600

    [UPDATE] SwagOverflowWPF/Themes/Generic.xaml - Settings flyout IsOpen binding, window button and icon usage

--- SwagOverflowWPF/Window/WindowSettingCollection.cs
+++ SwagOverflowWPF/Window/WindowSettingCollection.cs
@@ -11,12 +11,9 @@ namespace SwagOverflowWPF.Controls
         public WindowSettingCollection() : base()
         {
             this.Display = "Root";
-            //this["General"]["Theme"] = new SwagSettingViewModel<String>() { Value = "Temp" };
-            this["General"]["Theme"]["Base"] = new SwagSettingViewModel<String>() { Value = "Light", ItemsSource = new[] { "Light", "Dark" }, SettingType = SettingType.DropDown };
-            this["General"]["Theme"]["Accent"] = new SwagSettingViewModel<String>() { Value = "Blue", ItemsSource = new [] { "Red", "Green", "Blue", "Purple", "Orange", "Lime", "Emerald", "Teal", "Cyan", "Cobalt", "Indigo", "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna" }, SettingType = SettingType.DropDown };
-            //this["General"]["Theme2"] = new SwagSettingViewModel<String>() { Value = "Option3", ItemsSource = new[] { "Option1", "Option2", "Option3" }, SettingType = SettingType.DropDown };
-            //this["General"]["Nest"]["Theme"] = new SwagSettingViewModel<String>() { Value = "Option1", ItemsSource = new[] { "Option1", "Option2", "Option3" }, SettingType = SettingType.DropDown };
-            //this["General"]["Nest"]["Theme2"] = new SwagSettingViewModel<String>() { Value = "Option3", ItemsSource = new[] { "Option1", "Option2", "Option3" }, SettingType = SettingType.DropDown };
+            this["SettingsIsOpen"] = new SwagSettingViewModel<Boolean>() { GenericValue = true };
+            this["General"]["Theme"]["Base"] = new SwagSettingViewModel<String>() { GenericValue = "Light", ItemsSource = new[] { "Light", "Dark" }, SettingType = SettingType.DropDown };
+            this["General"]["Theme"]["Accent"] = new SwagSettingViewModel<String>() { GenericValue = "Blue", ItemsSource = new [] { "Red", "Green", "Blue", "Purple", "Orange", "Lime", "Emerald", "Teal", "Cyan", "Cobalt", "Indigo", "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna" }, SettingType = SettingType.DropDown };
 
             this["General"]["Theme"]["Base"].PropertyChanged += WindowSettingCollection_ThemePropertyChanged;
             this["General"]["Theme"]["Accent"].PropertyChanged += WindowSettingCollection_ThemePropertyChanged;
