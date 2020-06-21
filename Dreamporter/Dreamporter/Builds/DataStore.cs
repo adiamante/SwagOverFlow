163db8608fa9de918c0331872ab135f39812ee34
Desktop <dmnt123@gmail.com>
2020-06-21 18:35:16 -0500

    [NEW] Dreamporter/Dreamporter/Builds/DataStore.cs - Build class

---
+++ Dreamporter/Dreamporter/Builds/DataStore.cs
@@ -0,0 +1,33 @@
+﻿using SwagOverFlow.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dreamporter.Builds
+{
+    public abstract class DataStore : ViewModelBaseExtended
+    {
+        String _name;
+
+        #region Name
+        public String Name
+        {
+            get { return _name; }
+            set { SetValue(ref _name, value); }
+        }
+        #endregion Name
+    }
+
+    public class SqlConnectionStore : DataStore
+    {
+        String _connectionString;
+
+        #region ConnectionString
+        public String ConnectionString
+        {
+            get { return _connectionString; }
+            set { SetValue(ref _connectionString, value); }
+        }
+        #endregion ConnectionString
+    }
+}
